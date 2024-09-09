165c2a58c3e094a6166b87dc701d90e8d28f9b5d
jrvieirapr <105877022+jrvieirapr@users.noreply.github.com>
2024-09-09 13:44:33 +0000

    AnimeDetail

---
+++ src/components/AnimeDetail.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// useParams vai trazer pra nos o ID do anime
+import { useParams, Link } from 'react-router-dom';
+
+const AnimeDetail = () => {
+
+    //criar variavel que vai receber os animes
+    const [anime, setAnime] = useState(null)
+
+    //Pegar o id do anime, metodo destrutor
+    const { id } = useParams();
+
+    //UseEffect para carregar o anime pela id.
+    //usar [id] no final para executar toda vez que mudar
+    useEffect(() => {
+        //usa crase para criar uma interpolação
+        axios
+            .get(`https://api.jikan.moe/v4/anime/${id}`)
+            .then((response) => setAnime(response.data.data))
+            .catch((error) => console.log(error))
+    }, [id])
+    //  Retornar componente
+    return (
+        //Digitar codigo html dentro do react fragment
+        <React.Fragment>
+
+            <div className="container">
+                {
+                    anime ? (
+                        <div className="card">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <img src="{anime.images.jpg.large_image_url}"
+                                        alt="{anime.title}"
+                                        className="card-img"
+                                    />
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="card-body">
+                                        <h1 className="card-title">{anime.title}</h1>
+                                        <p className="card-text">{anime.synopsis}</p>
+                                        <Link to="/" 
+                                        className="btn btn-primary">
+                                            Voltar para o Home
+                                            </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <p>
+                            Carregando...
+                        </p>
+                    )
+                }
+            </div>
+
+        </React.Fragment>
+    )
+
+    //Fim
+}
+export default AnimeDetail;
+
